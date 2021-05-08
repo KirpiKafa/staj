9781e7515851d9bdd338ee56891e781a9747eb44
KirpiKafa <72442841+KirpiKafa@users.noreply.github.com>
2021-05-08 21:14:07 +0300

    restfull api bitti

--- restfull/app.js
+++ restfull/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser= require('body-parser');
+const cors = require('cors');
 require("dotenv/config");
+
+// middlewares
+app.use(cors());
+
+app.use(bodyParser.json());
 
 // İmport routers
 const postRoute = require('./routes/posts');
--- restfull/routes/posts.js
+++ restfull/routes/posts.js
@@ -1,14 +1,62 @@
-const express= require('express');
-const router= express.Router();
-const Post= require('../models/posts');
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/posts");
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
-} ); 
-
-router.post('/',(req,res) =>{
-    console.log(req.body);
+// Get back all the post
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
+// Submits a post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-module.exports=router;
+// specific post
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Delete post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// update a post
+router.patch("/:postId", (req, res) => {
+  try {
+    const uptatedPost = Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
